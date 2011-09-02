9942a36fa6d0a0afcc96883f5ed33e339815d851
Graham Tackley <graham.tackley@guardian.co.uk>
2011-09-02 11:45:03 +0100

    Added ability to automatically publish artifact to TeamCity

--- src/main/scala/com/gu/SbtDistPlugin.scala
+++ src/main/scala/com/gu/SbtDistPlugin.scala
@@ -6,13 +6,19 @@ import Keys._
 object SbtDistPlugin extends Plugin {
   val distConf = config("dist") hide
 
-  lazy val distFiles = TaskKey[Seq[(File, String)]]("dist-files")
-  lazy val distPath = SettingKey[File]("dist-path")
-  lazy val dist = TaskKey[File]("dist")
+  lazy val distFiles = TaskKey[Seq[(File, String)]]("dist-files", "Files to include in the distribution zip")
+  lazy val distPath = SettingKey[File]("dist-path", "Path to generate the distribution zip to")
+  lazy val distTeamCityIntegration = SettingKey[Boolean]("dist-teamcity-integration", "Ask TeamCity to publish zip")
+  lazy val dist = TaskKey[File]("dist", "Generate distribution zip file")
 
-  def distTask(src: Seq[(File, String)], dest: File, s: TaskStreams) = {
+  def distTask(src: Seq[(File, String)], dest: File, teamCityIntegration: Boolean, s: TaskStreams) = {
     s.log.info("writing distribution to: " + dest)
     IO.zip(src, dest)
+
+    if (teamCityIntegration) {
+      println("##teamcity[publishArtifacts '%s => .']" format dest.getAbsolutePath)
+    }
+
     dest
   }
 
@@ -32,13 +38,18 @@ object SbtDistPlugin extends Plugin {
     }
   }
 
+    // teamcity sets this environment variable when it runs scripts
+    private lazy val teamCityProjectName = Option(System.getenv("TEAMCITY_PROJECT_NAME"))
+    private lazy val isRunningUnderTeamCity= teamCityProjectName.isDefined
+
 
   val distSettings: Seq[Project.Setting[_]] = Seq(
     ivyConfigurations += distConf,
+    distTeamCityIntegration := isRunningUnderTeamCity,
     distFiles <<= deployLibFiles,
 		distPath <<= (target) { (target) => target / "dist" / "artifacts.zip" },
     managedClasspath in dist <<= (classpathTypes, update) map { (ct, report) => Classpaths.managedJars(distConf, ct, report) },
-    dist <<= (distFiles, distPath, streams) map distTask
+    dist <<= (distFiles, distPath, distTeamCityIntegration, streams) map distTask
 	)
 
 }
